caaa5aaf84b5345fe2b2f0007f269fb379b895df
jim <jbmbroomfield@gmail.com>
2021-10-08 12:31:07 +1100

    Created user#mood

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,14 @@ class User < ApplicationRecord
 
     has_secure_password
 
+    def mood
+        if self.happiness > self.nausea
+            'happy'
+        elsif self.happiness < self.nausea
+            'sad'
+        else
+            'neutral'
+        end
+    end
+
 end
