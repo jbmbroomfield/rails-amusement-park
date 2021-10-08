f2f89b23d5eee145b6a675a781ad66beaa85f7e9
jim <jbmbroomfield@gmail.com>
2021-10-08 12:28:27 +1100

    Added default value of false to user.admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_08_011727) do
+ActiveRecord::Schema.define(version: 2021_10_08_012506) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2021_10_08_011727) do
     t.integer "happiness"
     t.integer "tickets"
     t.integer "height"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
