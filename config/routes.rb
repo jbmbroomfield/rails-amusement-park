c04c5ad2d053eb5b23147504373d5074bf08deb8
jim <jbmbroomfield@gmail.com>
2021-10-08 16:55:30 +1100

    Created signin and signout routes, and added #destroy to SessionsController

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   
   root 'pages#home'
 
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  post '/signout', to: 'sessions#destroy'
+
   resources :users, only: [:new, :create, :show]
 
 end
