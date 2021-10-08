436153a8ca4cf22ccf9eea6a6d75e68d75c9d547
jim <jbmbroomfield@gmail.com>
2021-10-08 15:16:30 +1100

    Implemented ride#take_ride

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -3,4 +3,8 @@ class Attraction < ApplicationRecord
     has_many :rides
     has_many :users, through: :rides
 
+    def to_s
+        return name
+    end
+
 end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,4 +3,24 @@ class Ride < ApplicationRecord
     belongs_to :user
     belongs_to :attraction
 
+    def take_ride
+        get_error_message
+        @error_message.length > 0 ? "Sorry.#{@error_message}" : successfully_take_ride
+    end
+
+    private
+
+    def get_error_message
+        @error_message = ''
+        @error_message += " You do not have enough tickets to ride the #{attraction}." if user.tickets < attraction.tickets
+        @error_message += " You are not tall enough to ride the #{attraction}." if user.height < attraction.min_height
+    end
+
+    def successfully_take_ride
+        user.tickets -= attraction.tickets
+        user.nausea += attraction.nausea_rating
+        user.happiness += attraction.happiness_rating
+        user.save
+    end
+
 end
